26221ae7c9700b7f3d2b0420883ce4125bee0118
Martin Wicke <martin.wicke@gmail.com>
2012-08-22 13:43:38 -0700

    added amap -- work like python amap

---
+++ array/amap.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <other/core/array/Array.h>
+
+namespace other {
+
+template<class F,class A> auto amap(const F& f, const A& a)
+  -> decltype(Array<decltype(f(a[0])),A::d>()) {
+  
+  typedef decltype(Array<decltype(f(a[0])),A::d>()) ret;
+
+  ret b(a.sizes(), false);
+  
+  for (int i = 0; i < a.flat.size(); ++i) {
+    b.flat[i] = f(a.flat[i]);
+  }
+  
+  return b;
+  
+}
+
+}
