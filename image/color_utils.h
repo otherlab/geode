d180e13f44d1711bda0d908131c5ac09e0022f8a
Geoffrey Irving <irving@naml.us>
2012-09-08 14:19:52 -0700

    Expose wheel_color to python

--- image/color_utils.h
+++ image/color_utils.h
@@ -1,21 +1,21 @@
 #pragma once
 
 #include <other/core/vector/Vector.h>
-
 namespace other {
   
-  Vector<real, 3> hsv_to_rgb(Vector<real, 3> cin) OTHER_EXPORT;
-  Vector<real, 3> rgb_to_hsv(Vector<real, 3> const &cin) OTHER_EXPORT;
-    
-  inline Vector<unsigned char,4> to_byte_color(Vector<real, 3> const &c, real alpha = 1) {
-    return Vector<unsigned char,4>(vec(255*c[0],255*c[1],255*c[2],255*alpha));
-  }
+Vector<real,3> hsv_to_rgb(Vector<real,3> cin) OTHER_EXPORT;
+Vector<real,3> rgb_to_hsv(Vector<real,3> const &cin) OTHER_EXPORT;
   
-  inline Vector<real, 3> wheel_color(real hue) {
-    return hsv_to_rgb(Vector<real, 3>(hue, 1., 1.));
-  }
-  
-  inline Vector<real, 3> wheel_color(int i, int n) {
-    return wheel_color(real(i)/n);
-  };
+static inline Vector<unsigned char,4> to_byte_color(Vector<real,3> const &c, real alpha = 1) {
+  return Vector<unsigned char,4>(vec(255*c[0],255*c[1],255*c[2],255*alpha));
+}
+
+static inline Vector<real,3> wheel_color(real hue) {
+  return hsv_to_rgb(Vector<real,3>(hue,1,1));
+}
+
+static inline Vector<real,3> wheel_color(int i, int n) {
+  return wheel_color(real(i)/n);
+}
+
 }
--- image/module.cpp
+++ image/module.cpp
@@ -5,6 +5,7 @@
 using namespace other;
 
 void wrap_image() {
-    OTHER_WRAP(Image)
-    OTHER_WRAP(mov)
+  OTHER_WRAP(Image)
+  OTHER_WRAP(mov)
+  OTHER_WRAP(color_utils)
 }
