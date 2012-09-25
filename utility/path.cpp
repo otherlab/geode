cdb57a5c9906bb9a6fc1a1808a46d94cb80ab0aa
Geoffrey Irving <irving@naml.us>
2012-09-25 10:16:36 -0700

    utility/path: Add a few more path manipulation utilities

--- utility/path.cpp
+++ utility/path.cpp
@@ -4,18 +4,35 @@
 namespace other {
 namespace path {
 
+string join(const string& p, const string& q) {
+  return p+sep+q;
+}
+
 string extension(const string& path) {
   for (int i=path.size()-1;i>=0;i--) {
-#ifdef _WIN32
-    if (path[i]=='/' || path[i]=='\\')
-#else
-    if (path[i]=='/')
-#endif
+    if (is_sep(path[i]))
       break;
-    else if (path[i]=='.')
+    if (path[i]=='.')
       return path.substr(i);
   }
   return string();
+}
+
+string remove_extension(const string& path) {
+  for (int i=path.size()-1;i>=0;i--) {
+    if (is_sep(path[i])) 
+      break;
+    if (path[i]=='.')
+      return path.substr(0,i); 
+  }
+  return path;
+}
+
+string basename(const string& path) {
+  for (int i=path.size()-1;i>=0;i--)
+    if (is_sep(path[i]))
+      return path.substr(i+1);
+  return path;
 }
 
 }
--- utility/path.h
+++ utility/path.h
@@ -11,7 +11,25 @@ namespace path {
 
 using std::string;
 
+#ifdef _WIN32
+const char sep = '\\';
+static inline bool is_sep(char c) {
+  return c=='\\' || c=='/';
+}
+#else
+const char sep = '/';
+static inline bool is_sep(char c) {
+  return c==sep;
+}
+#endif
+
+string join(const string& p, const string& q) OTHER_EXPORT;
+
 string extension(const string& path) OTHER_EXPORT;
+
+string remove_extension(const string& path) OTHER_EXPORT;
+
+string basename(const string& path) OTHER_EXPORT;
 
 }
 }
