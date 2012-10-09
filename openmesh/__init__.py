662cb003aec55515d7ba27152783caf0df726190
Martin Wicke <martin.wicke@gmail.com>
2012-10-09 13:38:34 -0700

    fixed python include

--- openmesh/__init__.py
+++ openmesh/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-import libother_openmesh
-from libother_openmesh import *
+import libother_core
+from libother_core import *
 from numpy import inf
 
 def decimate(mesh, max_collapses=(1<<31)-1, max_angle_error=90, max_quadric_error=inf):
-  return libother_openmesh.decimate(mesh,max_collapses,max_angle_error,max_quadric_error)
+  return libother_core.decimate(mesh,max_collapses,max_angle_error,max_quadric_error)
