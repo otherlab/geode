4348a4a01ca717986ed5ba6e7c088af8304c520e
Martin Wicke <martin.wicke@gmail.com>
2012-10-11 17:05:22 -0700

    fixed python interface for decimate

--- openmesh/__init__.py
+++ openmesh/__init__.py
@@ -4,5 +4,5 @@ import libother_core
 from libother_core import *
 from numpy import inf
 
-def decimate(mesh, max_collapses=(1<<31)-1, max_angle_error=90, max_quadric_error=inf):
-  return libother_core.decimate(mesh,max_collapses,max_angle_error,max_quadric_error)
+def decimate(mesh, max_collapses=(1<<31)-1, max_angle_error=90, max_quadric_error=inf, min_quality=1e-5,min_boundary_dot=.99):
+  return libother_core.decimate(mesh,max_collapses,max_angle_error,max_quadric_error,min_quality,min_boundary_dot)
