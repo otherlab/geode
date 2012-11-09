95a603b182be68505efbdb80909f09f306729ed2
Geoffrey Irving <irving@naml.us>
2012-11-09 11:02:29 -0800

    geometry/platonic: Move icosahedron_mesh and sphere_mesh to C++

--- geometry/module.cpp
+++ geometry/module.cpp
@@ -5,10 +5,11 @@
 using namespace other;
 
 void wrap_geometry() {
-    OTHER_WRAP(implicit)
-    OTHER_WRAP(frame_implicit)
-    OTHER_WRAP(analytic_implicit)
-    OTHER_WRAP(box_tree)
-    OTHER_WRAP(particle_tree)
-    OTHER_WRAP(simplex_tree)
+  OTHER_WRAP(implicit)
+  OTHER_WRAP(frame_implicit)
+  OTHER_WRAP(analytic_implicit)
+  OTHER_WRAP(box_tree)
+  OTHER_WRAP(particle_tree)
+  OTHER_WRAP(simplex_tree)
+  OTHER_WRAP(platonic)
 }
