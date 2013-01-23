fdc9710be63fd8c7ea4f4f96d051ac8f243b29cb
Otherlab <info@otherlab.com>
2013-01-23 10:07:38 -0800

    array/alloca.h: Fix for Windows

--- array/alloca.h
+++ array/alloca.h
@@ -2,7 +2,13 @@
 #pragma once
 
 #include <other/core/array/RawArray.h>
+#ifdef _WIN32
+#include <malloc.h>
+#define OTHER_ALLOCA _alloca
+#else
 #include <alloca.h>
+#define OTHER_ALLOCA alloca
+#endif
 
 // OTHER_RAW_ALLOCA(count,Type) stack allocates an uninitialized buffer using alloca, and returns it as a RawArray.
 // IMPORTANT: On Windows, the size argument is evaluated twice, since there is no equivalent to gcc's
@@ -10,8 +16,8 @@
 #ifdef __GNUC__
 #define OTHER_RAW_ALLOCA(count,...) ({ \
   const int _count = (count); \
-  ::other::RawArray<__VA_ARGS__>(_count,(__VA_ARGS__*)alloca(sizeof(__VA_ARGS__)*_count)); })
+  ::other::RawArray<__VA_ARGS__>(_count,(__VA_ARGS__*)OTHER_ALLOCA(sizeof(__VA_ARGS__)*_count)); })
 #else // Fallback version that evaluates the count argument twice
 #define OTHER_RAW_ALLOCA(count,...) \
-  (::other::RawArray<__VA_ARGS__>((count),(__VA_ARGS__*)alloca(sizeof(__VA_ARGS__)*(count))))
+  (::other::RawArray<__VA_ARGS__>((count),(__VA_ARGS__*)OTHER_ALLOCA(sizeof(__VA_ARGS__)*(count))))
 #endif
