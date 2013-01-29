c6d3d5eacdf2be8f27dcacaf1a4f710447b686a3
Geoffrey Irving <irving@naml.us>
2013-01-29 09:16:43 -0800

    value/convert.h: Simplify

--- value/convert.h
+++ value/convert.h
@@ -10,13 +10,9 @@ namespace other {
 
 #ifdef OTHER_PYTHON
 
-template<class T> inline T convert_helper(const ValueRef<Ptr<>>& value) {
+template<class T> static T convert_helper(const ValueRef<Ptr<>>& value) {
   Ptr<> v = value();
   return from_python<T>(v ? v.get() : Py_None);
-}
-
-template<class T> inline ValueRef<T> convert(const ValueRef<Ptr<>>& value) {
-  return cache(convert_helper<T>,value);
 }
 
 template<class T> struct FromPython<ValueRef<T>>{static ValueRef<T> convert(PyObject* object) {
@@ -24,7 +20,7 @@ template<class T> struct FromPython<ValueRef<T>>{static ValueRef<T> convert(PyOb
   if (const Value<T>* exact = base.cast<T>())
     return ValueRef<T>(*exact);
   if (const Value<Ptr<>>* python = base.cast<Ptr<>>())
-    return other::convert<T>(*python);
+    return cache(convert_helper<T>,ValueRef<Ptr<>>(*python));
   throw TypeError(format("can't convert '%s' to '%s'",typeid(base).name(),typeid(Value<T>).name()));
 }};
 
