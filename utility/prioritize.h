61fc78b5edaef2945c9fcbebd5dc25b1b9413c96
Martin Wicke <martin.wicke@gmail.com>
2013-02-01 10:30:32 -0800

    added output operator for Prioritize

--- utility/prioritize.h
+++ utility/prioritize.h
@@ -1,15 +1,17 @@
 #pragma once
 
+#include <iostream>
+
 namespace other {
 
 template<class A, class P = double>
 struct Prioritize {
   A a;
   P p;
-  
+
   Prioritize(A const &a, P const &p)
     : a(a), p(p) {}
-  
+
   bool operator<(const Prioritize<A,P>& b) const {
     return p < b.p;
   }
@@ -22,6 +24,11 @@ struct Prioritize {
 template<class A, class B>
 inline Prioritize<A,B> prioritize(const A& a, const B& b) {
   return Prioritize<A,B>(a, b);
-}  
+}
+
+template<class A, class B>
+std::ostream &operator<<(std::ostream &os, Prioritize<A,B> const &p) {
+  return os << p.p << ":" << p.a;
+}
 
 }
