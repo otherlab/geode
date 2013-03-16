cbc863c2ca870f30d7c5ac731cc8482fc129927c
Geoffrey Irving <irving@naml.us>
2013-03-16 16:49:56 -0700

    utility/module: Wrap integer_log for Python

--- math/module.cpp
+++ math/module.cpp
@@ -1,9 +1,12 @@
 //#####################################################################
 // Module math
 //#####################################################################
+#include <other/core/math/integer_log.h>
 #include <other/core/python/wrap.h>
+using namespace other;
 
 void wrap_math() {
   OTHER_WRAP(uint128)
   OTHER_WRAP(numeric_limits)
+  OTHER_FUNCTION_2(integer_log,static_cast<int(*)(uint64_t)>(integer_log));
 }
