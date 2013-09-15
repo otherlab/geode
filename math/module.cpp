bf39744ce9ffab0d2846a4a54f9a7fc312fe5507
Geoffrey Irving <irving@naml.us>
2013-09-15 13:20:01 -0700

    math/module: Expose popcount and min_bit to Python

--- math/module.cpp
+++ math/module.cpp
@@ -2,11 +2,14 @@
 // Module math
 //#####################################################################
 #include <other/core/math/integer_log.h>
+#include <other/core/math/popcount.h>
 #include <other/core/python/wrap.h>
 using namespace other;
 
 void wrap_math() {
   OTHER_WRAP(uint128)
   OTHER_WRAP(numeric_limits)
-  OTHER_FUNCTION_2(integer_log,static_cast<int(*)(uint64_t)>(integer_log));
+  OTHER_FUNCTION_2(integer_log,static_cast<int(*)(uint64_t)>(integer_log))
+  OTHER_FUNCTION_2(popcount,static_cast<int(*)(uint64_t)>(popcount))
+  OTHER_FUNCTION_2(min_bit,static_cast<uint64_t(*)(uint64_t)>(min_bit))
 }
