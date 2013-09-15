a67f1b7d7b9b62d5e27709939d0923e385197bf5
Geoffrey Irving <irving@naml.us>
2013-09-15 13:20:37 -0700

    Register: Add an affine_register function

--- vector/Register.h
+++ vector/Register.h
@@ -8,6 +8,11 @@
 #include <other/core/vector/forward.h>
 namespace other {
 
-OTHER_CORE_EXPORT Frame<Vector<real,2> > rigid_register(RawArray<const Vector<real,2> > X0,RawArray<const Vector<real,2> > X1, RawArray<const real> M = RawArray<const real>());
-OTHER_CORE_EXPORT Frame<Vector<real,3> > rigid_register(RawArray<const Vector<real,3> > X0,RawArray<const Vector<real,3> > X1, RawArray<const real> M = RawArray<const real>());
+OTHER_CORE_EXPORT Frame<Vector<real,2>> rigid_register(RawArray<const Vector<real,2>> X0, RawArray<const Vector<real,2>> X1, RawArray<const real> mass = RawArray<const real>());
+OTHER_CORE_EXPORT Frame<Vector<real,3>> rigid_register(RawArray<const Vector<real,3>> X0, RawArray<const Vector<real,3>> X1, RawArray<const real> mass = RawArray<const real>());
+
+// Find the best affine transform from X0 to X1.  Warning: undetermined problems will give poor results.
+OTHER_CORE_EXPORT Matrix<real,3> affine_register(RawArray<const Vector<real,2>> X0, RawArray<const Vector<real,2>> X1, RawArray<const real> mass = RawArray<const real>());
+OTHER_CORE_EXPORT Matrix<real,4> affine_register(RawArray<const Vector<real,3>> X0, RawArray<const Vector<real,3>> X1, RawArray<const real> mass = RawArray<const real>());
+
 }
