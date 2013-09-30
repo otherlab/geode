af5c06ffaffef445a498b05b3f4bb0d5e3726742
Geoffrey Irving <irving@naml.us>
2013-09-30 09:46:18 -0700

    ArrayPlusScalar: Be more const flexible

--- array/ArrayPlusScalar.h
+++ array/ArrayPlusScalar.h
@@ -39,7 +39,7 @@ public:
 
 template<class T1,class T2,class Enable=void> struct ArrayPlusScalarValid:public mpl::false_{};
 template<class T1,class T2> struct ArrayPlusScalarValid<T1,T2,typename mpl::if_<mpl::true_,void,typename Sum<T1,T2>::type>::type>:public
-  mpl::or_<boost::is_same<T1,T2>,IsScalar<T1> >{};
+  mpl::or_<boost::is_same<typename boost::remove_const<T1>::type,typename boost::remove_const<T2>::type>,IsScalar<T1> >{};
 
 template<class T1,class T2,class TArray2> static inline typename boost::enable_if<ArrayPlusScalarValid<T1,T2>,ArrayPlusScalar<T1,TArray2> >::type
 operator+(const T1& c, const ArrayBase<T2,TArray2>& array) {
