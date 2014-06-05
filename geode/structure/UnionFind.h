78eb1f8d77056dce3e58ccf64c9c7f1f4f207bab
Geoffrey Irving <irving@naml.us>
2014-06-05 15:26:58 -0700

    Speed up UnionFind by around 30%
    This replaces path compression with path halving, and reuses the parents
    array to store ranks.  The latter avoids an extra array, but strangely
    did not help by itself.

--- geode/structure/UnionFind.h
+++ geode/structure/UnionFind.h
@@ -1,22 +1,27 @@
-//#####################################################################
-// Class UnionFind
-//#####################################################################
+// Union-find algorithm for disjoint sets
 #pragma once
+
+// Our implementation uses path halving rather than path compression, and reuses the parents
+// array to store ranks as well.  This combination is LRPH-MS in the terminology of
+//
+//   Patwary, Blair, Manne (2010), "Experiments on Union-Find Algorithms for the Disjoint-Set Data Structure".
+//
+// Although Patwary et al. claim that Rem is actually faster despite its inferior asymptotic
+// complexity.  Our experients did not confirm this: Rem switched to slower for sparse random
+// graphs between 10M and 100M vertices.  Therefore, we stick with reassuring O(n alpha(n)).
 
 #include <geode/structure/Tuple.h>
 #include <geode/array/ConstantMap.h>
 #include <geode/array/Array.h>
 namespace geode {
 
-template<class Derived,class Parents,class Ranks>
-struct UnionFindBase {
-  typedef typename Ranks::Element Rank;
-
+template<class Parents> struct UnionFindBase {
+  // For roots, parents[i] = -rank-1
   mutable Parents parents;
-  Ranks ranks;
 
 protected:
-  UnionFindBase() {} // It's the derived class's responsibility to initialize parents and ranks
+  // It's the derived class's responsibility to initialize parents
+  UnionFindBase() {}
 public:
 
   int size() const {
@@ -25,56 +30,68 @@ public:
 
   void clear_connectivity() {
     parents.fill(-1);
-    ranks.fill(0);
   }
 
   bool is_root(const int i) const {
     return parents[i]<0;
   }
 
-  bool same(const int i,const int j) const {
+  bool same(const int i, const int j) const {
     return find(i)==find(j);
   }
 
-  int find(const int i) const {
-    int root = find_without_path_compression(i);path_compress(i,root);
-    return root;
-  }
-
-  int merge(const int i,const int j) {
-    int root_i = find_without_path_compression(i),
-        root_j = find_without_path_compression(j);
-    int root = ranks[root_i]>=ranks[root_j]?root_i:root_j;
-    path_compress(i,root);
-    path_compress(j,root);
-    if (ranks[root_i]==ranks[root_j] && root_i!=root_j) ranks[root]++;
-    return root;
-  }
-
-  template<class TArray> int merge(const TArray& indices) {
-    STATIC_ASSERT_SAME(typename TArray::Element,int);
-    int root = find_without_path_compression(indices[0]);
-    Rank max_rank = ranks[root];
-    bool max_tie=false;
-    for (int i=1;i<indices.size();i++) {
-      int root_i = find_without_path_compression(indices[i]);
-      if (max_rank<ranks[root_i]) {
-        max_rank = ranks[root_i];
-        root = root_i;
-        max_tie = false;
-      } else if (max_rank==ranks[root_i] && root_i!=root)
-        max_tie=true;
+  int find(int i) const {
+    // Use path halving rather than full path compression for speed.
+    // Path halving changes x -> y -> z -> ... to x,y -> z -> ...
+    for (;;) {
+      const int pi = parents[i];
+      if (pi < 0)
+        return i;
+      const int ppi = parents[pi];
+      if (ppi < 0)
+        return pi;
+      parents[i] = ppi;
+      i = ppi;
     }
-    for (int i=0;i<indices.size();i++) path_compress(indices[i],root);
-    if (max_tie) ranks[root]++;
-    return root;
+  }
+
+  int merge(const int i, const int j) {
+    return merge_roots(find(i),find(j));
+  }
+
+  // Convenience merging for vectors of indices
+  int merge(const Vector<int,1> v) {
+    return find(v.x);
+  }
+  int merge(const Vector<int,2> v) {
+    return merge(v.x,v.y);
+  }
+  int merge(const Vector<int,3> v) {
+    const int xy = merge(v.x,v.y);
+    return merge_roots(xy,find(v.z));
+  }
+  int merge(const Vector<int,4> v) {
+    const int xy = merge(v.x,v.y);
+    const int zw = merge(v.z,v.w);
+    return merge_roots(find(xy),zw);
+  }
+
+  // Merge any nonempty list of indices
+  template<class TA> int merge(const TA& indices) {
+    STATIC_ASSERT_SAME(typename TA::Element,int);
+    const int n = indices.size();
+    int r = find(indices[0]);
+    for (int i=1;i<n;i++)
+      r = merge_roots(r,find(indices[i]));
+    return r;
   }
 
   void merge(const UnionFindBase& union_find) {
     assert(size()==union_find.size());
     for (int i=0;i<size();i++) {
-      int j = union_find.parents[i];
-      if (j>=0) merge(i,j);
+      const int j = union_find.parents[i];
+      if (j >= 0)
+        merge(i,j);
     }
   }
 
@@ -86,24 +103,20 @@ public:
   }
 
 private:
-  int find_without_path_compression(const int i) const {
-    int j = i;
-    while (parents[j]>=0) j = parents[j];
-    return j;
-  }
-
-  void path_compress(const int i,const int root) const {
-    int j = i;
-    while (j!=root) {
-      int parent = parents[j];
-      parents[j] = root;
-      j = parent;
-      if (j<0) break;
+  int merge_roots(int i, int j) {
+    assert(parents[i]<0 && parents[j]<0);
+    if (i != j) {
+      if (parents[i] > parents[j])
+        swap(i,j);
+      else if (parents[i] == parents[j])
+        parents[i]--;
+      parents[j] = i;
     }
+    return i; 
   }
 };
 
-class UnionFind : public UnionFindBase<UnionFind,Array<int>,Array<unsigned char> > {
+class UnionFind : public UnionFindBase<Array<int>> {
 public:
   explicit UnionFind(const int entries=0) {
     initialize(entries);
@@ -111,23 +124,22 @@ public:
 
   void initialize(const int entries) {
     parents.copy(ConstantMap<int>(entries,-1));
-    ranks.copy(ConstantMap<Rank>(entries,0));
   }
 
   int add_entry() {
-    parents.append(-1);
-    return ranks.append(0);
+    return parents.append(-1);
   }
 
   // Okay for map to yield invalid indices for isolated elements
-  template<class TArray> void mapped_merge(const UnionFind& union_find,const TArray& map) {
+  template<class TA> void mapped_merge(const UnionFind& union_find, const TA& map) {
     for (int i=0;i<union_find.size();i++) {
-      int root = union_find.find(i);
-      if (i!=root) merge(map(i),map(root));
+      const int root = union_find.find(i);
+      if (i != root)
+        merge(map(i),map(root));
     }
   }
 
-  void forest_edges(Array<Tuple<int,int> >& pairs) const {
+  void forest_edges(Array<Tuple<int,int>>& pairs) const {
     pairs.clear();
     for (int i=0;i<size();i++) {
       int j = find(i);
@@ -135,13 +147,13 @@ public:
     }
   }
 
-  void merge_forest_edges(RawArray<const Tuple<int,int> > pairs) {
+  void merge_forest_edges(RawArray<const Tuple<int,int>> pairs) {
     for (int i=0;i<pairs.m;i++)
       merge(pairs(i).x,pairs(i).y);
   }
 };
 
-template<int n> struct SmallUnionFind : public UnionFindBase<SmallUnionFind<n>,Vector<signed char,n>,Vector<unsigned char,n> > {
+template<int n> struct SmallUnionFind : public UnionFindBase<Vector<signed char,n>> {
   static_assert(0<=n && n<=128,"Large union finds should use UnionFind, not SmallUnionFind");
   
   SmallUnionFind() {
