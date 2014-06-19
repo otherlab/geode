9e7daf353331ff5d354274773d654355ffbb9c86
Martin Wicke <martin.wicke@gmail.com>
2014-06-19 00:14:52 -0700

    fix lower_hull and expose to python

--- geode/mesh/lower_hull.cpp
+++ geode/mesh/lower_hull.cpp
@@ -34,12 +34,16 @@ struct Toward {
   }
 };
 
-Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, Array<TV> const &pos, const TV up, const T overhang, const T ground_offset) {
+Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, Array<TV> const &pos, TV up, const T ground_offset) {
+
+  // TODO: this doesn't work properly.
+  T overhang = 0.;
+  up.normalize();
 
   Ref<MutableTriangleTopology> mesh = new_<MutableTriangleTopology>(imesh);
   FieldId<TV,VertexId> pos_id = mesh->add_field(Field<TV,VertexId>(pos), vertex_position_id);
 
-  // color faces
+  // classify faces
   Field<bool, FaceId> toward = Toward(mesh, mesh->field(pos_id), up, overhang?sin(M_PI/180.*overhang):0.).as_field();
 
   // make sure we include space for deleted faces
@@ -68,6 +72,14 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
 
   // for all components that are toward, add faces to our solution mesh
   auto new_mesh = new_<MutableTriangleTopology>();
+  FieldId<TV,VertexId> new_pos_id = new_mesh->add_vertex_field<TV>(vertex_position_id);
+  GEODE_ASSERT(new_pos_id == pos_id);
+
+  // we'll extend downward at least until the minimum value along up, or ground_offset, whichever is lower
+  real min_ground = ground_offset;
+  for (auto x : mesh->field(pos_id).flat) {
+    min_ground = min(min_ground, dot(x, up));
+  }
 
   int component_idx = 0;
   for (auto p : component_faces) {
@@ -80,7 +92,8 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
     component_mesh.split_nonmanifold_vertices();
 
     // compute the boundary loops (before we add the mirrored faces)
-    auto boundary_loops = component_mesh.boundary_loops();
+    // store vertices because the boundary halfedges will change
+    auto boundary_loops = amap([&](HalfedgeId h){return component_mesh.src(h);}, component_mesh.boundary_loops());
 
     // add vertices and remember the correspondence of vertices (simple offset)
     int voffset = component_mesh.n_vertices();
@@ -89,7 +102,7 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
       assert(nv.idx() == i + voffset);
       // flatten to ground_offset+epsilon
       TV x = component_mesh.field(pos_id)[VertexId(i)];
-      TV ground_pos = x + (- dot(x, up) + ground_offset - component_idx) * up;
+      TV ground_pos = x + (- dot(x, up) + min_ground - component_idx) * up;
       component_mesh.field(pos_id)[nv] = ground_pos;
     }
 
@@ -97,7 +110,7 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
     auto oldfaces = component_mesh.faces();
     int foffset = component_mesh.n_faces();
     for (auto f : oldfaces) {
-      Vector<VertexId,3> verts = component_mesh.vertices(f);
+      Vector<VertexId,3> verts = Vector<VertexId,3>::map([=](VertexId x){ return VertexId(x.id + voffset); }, component_mesh.vertices(f));
       FaceId nf = component_mesh.add_face(verts.xzy());
       assert(nf.idx() == f.idx() + foffset);
     }
@@ -105,12 +118,12 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
     // make side faces
     for (auto loop : boundary_loops) {
       for (int i = 0, j = loop.size()-1; i < loop.size(); j = i++) {
-        VertexId vi = component_mesh.src(loop[i]);
-        VertexId vj = component_mesh.src(loop[j]);
+        VertexId vi = loop[i];
+        VertexId vj = loop[j];
         VertexId vim = VertexId(vi.idx()+voffset);
         VertexId vjm = VertexId(vj.idx()+voffset);
-        component_mesh.add_face(vec(vi, vj, vjm));
-        component_mesh.add_face(vec(vi, vjm, vim));
+        component_mesh.add_face(vec(vj, vi, vjm));
+        component_mesh.add_face(vec(vjm, vi, vim));
       }
     }
 
@@ -135,11 +148,8 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
   // Then rotate that box back such that z == up.
   auto R = Rotation<TV>::from_rotated_vector(up, vec(0, 0, 1.));
 
-  // reduce to 8 points to be considered
-  auto corners = bounding_box(vertices).corners();
-
-  // rotate points
-  corners.copy(amap([=](TV v){ return R*v; }, corners));
+  // reduce to 8 points to be considered, and rotate those
+  auto corners = amap([&](TV v){ return R*v; }, bounding_box(vertices).corners());
 
   // compute bbox again, slightly inflated
   auto aabb = bounding_box(corners).thickened(1.);
@@ -151,13 +161,12 @@ Tuple<Ref<const TriangleSoup>, Array<TV>> lower_hull(TriangleSoup const &imesh, 
   // make inverted bbox mesh
   auto box = cube_mesh(aabb.max, aabb.min);
 
-  // rotate back
-  R = R.inverse();
-  box.y.copy(amap([=](TV v){ return R*v; }, box.y));
-
   // add
   faces.extend(box.x->elements + vertices.size());
-  vertices.extend(box.y);
+
+  // rotate back and add vertices
+  R = R.inverse();
+  vertices.extend(amap([&](TV v){ return R*v; }, box.y));
 
   // compute the union
   return split_soup(new_<TriangleSoup>(faces), vertices, 0);
--- geode/mesh/lower_hull.h
+++ geode/mesh/lower_hull.h
@@ -3,6 +3,6 @@
 
 namespace geode {
 
-Tuple<Ref<const TriangleSoup>, Array<Vector<real,3>>> lower_hull(TriangleSoup const &imesh, Array<Vector<real,3>> const &pos, const Vector<real,3> up, const real overhang, const real ground_offset);
+Tuple<Ref<const TriangleSoup>, Array<Vector<real,3>>> lower_hull(TriangleSoup const &imesh, Array<Vector<real,3>> const &pos, Vector<real,3> up, const real ground_offset);
 
 }
