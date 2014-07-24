76f8aa74e5fbd507fb2cbf276c55f5a287e0d203
Geoffrey Irving <irving@naml.us>
2014-07-24 11:19:38 -0700

    sqr: Add a monotonic version of sqr

--- geode/math/sqr.h
+++ geode/math/sqr.h
@@ -1,17 +1,18 @@
-//#####################################################################
-// Function sqr
-//#####################################################################
-//
-// Finds the square.
-//
-//#####################################################################
+// Square a number
 #pragma once
 
+#include <geode/math/copysign.h>
 namespace geode {
 
 template<class T> static inline auto sqr(const T& a)
   -> decltype(a*a) {
   return a*a;
+}
+
+// A monotonic version of sqr
+template<class T> static inline auto sign_sqr(const T& a)
+  -> decltype(a*a) {
+  return copysign(a*a,a);
 }
 
 }
