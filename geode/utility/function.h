c0345f2d2bd59c5d8656bcdfcd7d522dc23b2697
Geoffrey Irving <irving@naml.us>
2015-04-27 21:04:50 +0000

    utility: Correct __GNU__ check to __GNUC__

--- geode/utility/function.h
+++ geode/utility/function.h
@@ -5,7 +5,7 @@
 
 // If we're on clang, check for the right header directly.  If we're not,
 // any sufficient recently version of gcc should always have the right header.
-#if defined(__clang__) ? GEODE_HAS_INCLUDE(<functional>) : defined(__GNU__)
+#if defined(__clang__) ? GEODE_HAS_INCLUDE(<functional>) : defined(__GNUC__)
 #include <functional>
 #define GEODE_FUNCTION_NAMESPACE std
 #else
