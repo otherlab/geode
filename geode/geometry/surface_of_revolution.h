c1131ef4e26ef6ae39c0b1bd0e369423c92d5e1e
Forrest Green <greengadgetz@gmail.com>
2015-10-21 18:11:51 -0700

    Add surface_of_revolution and clean up a few small things
     * Add surface_of_revolution. (Needs better documentation and unit tests, but works for me)
     * Add asconstarray overload to convert std::array to RawArray (Now that std::array is part of the standard library, it might be good to rename make_array, asarray, and asconstarray to something less ambiguous)
     * Make 'normal' work for Vector<Vec3,3>
     * Mark some constants as constexpr. I used GEODE_CONSTEXPR_IF_NOT_MSVC (which might no longer be needed in Visual Studio 2015), but only tested with clang
     * Have a base version of AssertSame so that static_assert is actually triggered instead of a confusing message about missing template definitions

---
+++ geode/geometry/surface_of_revolution.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <geode/array/Array.h>
+#include <geode/vector/Vector.h>
+#include <geode/structure/tuple.h>
+namespace geode {
+
+// Generates mesh by rotating profile_rz around z axis
+// Returns triangles and positions of vertices
+
+GEODE_CORE_EXPORT Tuple<Array<Vector<int,3>>,Array<Vector<float,3>>>
+    surface_of_revolution(const RawArray<const Vector<float,2>> profile_rz, const int sides);
+
+GEODE_CORE_EXPORT Tuple<Array<Vector<int,3>>,Array<Vector<double,3>>>
+    surface_of_revolution(const RawArray<const Vector<double,2>> profile_rz, const int sides);
+
+} // geode namespace
