601081c8babd372aa3e5d6f34a119369e6635176
Forrest Green <greengadgetz@gmail.com>
2015-12-18 15:43:13 -0800

    Fix some unit tests to work when SSE is disabled

--- geode/math/sse.cpp
+++ geode/math/sse.cpp
@@ -1,18 +1,29 @@
 // SSE tests
 
 #include <geode/math/sse.h>
-#ifdef __SSE__
 #include <geode/python/wrap.h>
 namespace geode {
 
+static bool geode_sse_enabled() {
+#ifdef GEODE_SSE
+  return true;
+#else
+  return false;
+#endif
+}
+
+#ifdef GEODE_SSE
 static Vector<double,2> sse_pack_unpack(const Vector<double,2> a) {
   return unpack(pack<double>(a.x,a.y));
 }
+#endif
 
 }
 using namespace geode;
 
 void wrap_sse() {
-  GEODE_FUNCTION(sse_pack_unpack)  
-}
+  GEODE_FUNCTION(geode_sse_enabled)
+#ifdef GEODE_SSE
+  GEODE_FUNCTION(sse_pack_unpack)
 #endif
+}
