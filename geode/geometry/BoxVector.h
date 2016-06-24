94b41ce1e143930ea14bd62c290ea7ed0d92532b
Torrie Fischer <torrie@othermachine.co>
2016-06-24 12:23:30 -0700

    boxvector: implement -= and += for Vec

--- geode/geometry/BoxVector.h
+++ geode/geometry/BoxVector.h
@@ -115,6 +115,14 @@ public:
     min-=r.max;max-=r.min;return *this;
   }
 
+  Box<TV>& operator-=(const TV& r) {
+    min-=r;max-=r;return *this;
+  }
+
+  Box<TV>& operator+=(const TV& r) {
+    min+=r;max+=r;return *this;
+  }
+
   Box<TV> operator+(const Box<TV>& r) const {
     return Box<TV>(min+r.min,max+r.max);
   }
