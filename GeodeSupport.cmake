804466b8c17cc2ca666b30f24d66c32702ac7d14
Ruza Markov <ruza.markov@gmail.com>
2018-03-16 14:31:23 -0700

    add lib png to build

--- GeodeSupport.cmake
+++ GeodeSupport.cmake
@@ -101,7 +101,6 @@ macro(ADD_GEODE_MODULE _name)
         -Wno-format-security
         -Wno-attributes
         -Wno-unused-variable
-        -fPIC
     )
 
     CHECK_CXX_COMPILER_FLAG(-Wno-undefined-var-template COMPILER_CHECKS_UNDEFINED_VAR_TEMPLATE)
